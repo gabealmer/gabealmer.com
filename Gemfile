b6e2f3ee2a05617040066a66648faa6c7a7bbc1e
Gabor Almer <almergabor@gmail.com>
2013-01-02 02:01:53 +0100

    Add new relic

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source :rubygems
 
 gem 'sinatra', '1.3.3'
 gem 'thin'
+
+gem 'newrelic_rpm'
--- web.rb
+++ web.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+configure :production do
+  require 'newrelic_rpm'
+end
+
 get '/' do
   send_file File.expand_path('index.html', settings.public_folder)
 end
